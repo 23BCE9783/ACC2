28b8f83005a30765aebde107d25ce4df738773fc
23BCE9783 <kiran.23bce9783@vitapstudent.ac.in>
2026-01-21 05:14:16 +0000

    done

--- D20260121.java
+++ D20260121.java
@@ -1,1 +1,34 @@
+import java.util.*;
 
+class Main {
+    static void Span(int a[], int s[], int n) {
+        Stack<Integer> st = new Stack<>();
+        s[0] = 1;
+        st.push(0);
+        for (int i = 1; i < n; i++) {
+            while (!st.isEmpty() && a[i] > a[st.peek()]) {
+                st.pop();
+            }
+            s[i] = st.isEmpty() ? i + 1 : i - st.peek();
+            st.push(i);
+        }
+        System.out.println(Arrays.toString(s));
+    }
+
+    public static void main(String arf[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        int s[] = new int[n];
+        Span(a, s, n);
+        sc.close();
+    }
+}
+
+
+// Stock Span Brute force
+// SLL Swap the adjacent nodes
+// Make a PDF and Mail before 11pm
